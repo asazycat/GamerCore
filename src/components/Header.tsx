031693f85ae830d492982d7362f22f28913a9f85
Asad Jawaid <asazycat@gmail.com>
2023-09-04 08:50:24 +0100

    css changes

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header () {
+    return (
+        <nav>
+            <ul className="naviagtion">
+                <li>Feed</li>
+                <li>LFG</li>
+                <li>Clubs</li>
+                <li>Stream</li>
+            </ul>
+        </nav>
+    )
+}
