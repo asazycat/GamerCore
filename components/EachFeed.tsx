c3acf5973e141e9357b8535bc3a16e5088d24b64
Asad Jawaid <asazycat@gmail.com>
2023-09-06 10:07:53 +0100

    sortby function works for comments

--- components/EachFeed.tsx
+++ components/EachFeed.tsx
@@ -7,16 +7,23 @@ export default function EachFeed (props:{
     votes: number ,
     comment_amount: number,
     date: string
-}}) 
+},
+ 
+}) 
 {
 
     const {eachFeed} = props
+    
+   
     return (
         <li><div className="eachFeed">
+            <p className="postTitle">{eachFeed.post_title}</p>
             <p className="userPost">{eachFeed.user}</p>
             <p className="postContent">{eachFeed.post_content}</p>
-            <p className="votes">{eachFeed.votes}</p>
+            <p className="votes">{eachFeed.votes} Upvote</p>
+           
             <p className="date">{eachFeed.date}</p>
+            <p className="comment_amount">Number of comments {eachFeed.comment_amount}</p>
             </div></li>
     )
 }
